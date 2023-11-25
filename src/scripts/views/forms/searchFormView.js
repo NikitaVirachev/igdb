d5f1a95d9f599ef8b1e8888e1e66cc9438049dfd
NikitaVirachev <guardsolder@gmail.com>
2023-11-25 15:18:06 +0300

    refactor: remove old files

--- src/scripts/views/forms/searchFormView.js
+++
@@ -1,5 +0,0 @@
-import FormView from './formView.js';
-
-class SearchFormView extends FormView {}
-
-export default new SearchFormView('#search');
--- src/scripts/views/menu/loginView.js
+++
@@ -1,5 +0,0 @@
-import MenuView from './menuView.js';
-
-class LoginView extends MenuView {}
-
-export default new LoginView('#login');
